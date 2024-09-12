b8b43f9a07ee98db39d3dcd2d589329f3053c0b1
alexanderkeenethics <alexander.zarudnyak@keenethics.com>
2024-09-12 18:54:50 +0200

    main module refactor: - processCustomerPayments to avoid wrapping everything into an immediately invoked function; - async/await for better error handling and code readability; - separate constants API_URL and AUTHORIZATION_KEY to store API endpoint and authorization key; - Instead of checking res.ok inside a .then(), I have moved status check after receiving response. And, in case of an error, I have used the statusReject method to produce a meaningful error message; - update types;

--- src/payment-processor.ts
+++ src/payment-processor.ts
@@ -1,74 +1,90 @@
 import customers from './customer-list.json';
+import {type Customer, sendMessage, statusReject} from "./message-service";
+import {traceparent} from "tctx";
+
 const traceId = traceparent.make().toString();
 
-import { Customer, sendMessage, statusReject } from "./message-service";
-import { traceparent } from "tctx";
+const API_URL = "https://api.stripe.com/some-payment-endpoint";
+const AUTHORIZATION_KEY = `Bearer ${process.env.STRIPE_API_KEY}`;
 
 
-(async function() {
-	for (let i = 0; i < customers.length; i++) {
-		let customer = customers[i] as Customer;
-		
+type CustomersRecord = Customer & typeof customers[number];
+type PaymentMethod = CustomersRecord['paymentMethods'];
+
+const processCustomerPayments = async () => {
+	await Promise.all(customers.map(async (customer:CustomersRecord) => {
 		try {
-			await processPayment(customer).then(() => {
-				console.log('Successfully processed payment for customer', customer.id);
-			});
-		} catch (e) {
-			console.error('The payment failed to process:', e);
+			await processPayment(customer)
+					.then(() => console.log('Successfully processed payment for customer', customer.id));
+		} catch (err) {
+			console.error('The payment failed to process:', err);
 
-			if (e.message === 'Payment Failed') {
-				console.error('The payment failed to process:', e);
-				
-				const paymentMethod = customer.paymentMethods[customer.paymentMethods.defaultPaymentMethod];
-				let last4Digits = '';
-				
-				if (customer.paymentMethods.defaultPaymentMethod === 'card') {
-					last4Digits = paymentMethod.card.last4;
-				} else if (customer.paymentMethods.defaultPaymentMethod === 'usBankAccount') {
-					last4Digits = paymentMethod.usBankAccount.accountNumberLast4Digits;
-				} else if (customer.paymentMethods.defaultPaymentMethod == 'eu_pay_by_bank') {
-					last4Digits = paymentMethod.eu_pay_by_bank.iban_last_4;
-				}
-				
+			if (err.message === 'Payment Failed') {
+				console.error('The payment failed to process:', err);
+
+				const paymentMethod: PaymentMethod = customer.paymentMethods[customer.paymentMethods.defaultPaymentMethod];
+				let last4Digits = obtainLast4Digits(customer, paymentMethod);
+
 				sendMessage(customer, last4Digits);
 			}
 		}
+	}))
+}
+
+function obtainLast4Digits(customer: Customer, paymentMethod: PaymentMethod) {
+	let last4Digits = '';
+
+	switch (customer.paymentMethods.defaultPaymentMethod) {
+		case 'card':
+			last4Digits = paymentMethod.card.last4.toString();
+			break;
+		case 'usBankAccount':
+			last4Digits = paymentMethod.usBankAccount.accountNumberLast4Digits;
+			break;
+		case 'eu_pay_by_bank':
+			last4Digits = paymentMethod.eu_pay_by_bank.iban_last_4;
 	}
-})();
 
+	return last4Digits;
+}
 
-function processPayment(customer: Customer) {
-	return fetch('https://api.stripe.com/some-payment-endpoint', {
-		method: 'POST',
-		headers: {
-			"Content-Type": "application/json",
-			Traceparent: traceId,
-			Authorization: 'Bearer ' + process.env.STRIPE_API_KEY
-		},
-		body: JSON.stringify({
-			customerId: customer.id,
-			paymentMethod: customer.paymentMethods[customer.paymentMethods.defaultPaymentMethod],
-			amount: getCustomerPaymentAmount(customer.id)
-		})
-	}).then((res) => {
-		if (res.ok) {
-			return res.json().then((data) => {
-				return data;
-			});
-		} else if (res.status == 403) {
-			return Promise.reject(statusReject(res));
-		} else {
-			return res.json().then((data) => {
-				return Promise.reject(data);
-			});
+async function processPayment(customer: Customer): Promise<any> {
+	const body = {
+		customerId: customer.id,
+		paymentMethod: customer.paymentMethods[customer.paymentMethods.defaultPaymentMethod],
+		amount: getCustomerPaymentAmount(customer.id)
+	}
+
+	try {
+		const response = await fetch(API_URL, {
+			method: 'POST',
+			headers: {
+				"Content-Type": "application/json",
+				Traceparent: traceId,
+				Authorization: AUTHORIZATION_KEY
+			},
+			body: JSON.stringify(body)
+		});
+
+		const data = await response.json();
+
+		if (!response.ok) {
+			throw statusReject(response);
 		}
-	}).catch((err) => {
+
+		return data;
+	} catch (err) {
 		console.error('Error calling Stripe payment API:', err);
-		return Promise.reject(err);
-	});
+		throw err;
+	}
 }
 
 function getCustomerPaymentAmount(customerId: number) {
 	const amount = Math.floor(Math.random() * (100 - 50 + 1) + 50) + Math.random();
 	return amount.toFixed(2);
 }
+
+
+(async function () {
+	await processCustomerPayments();
+})();
